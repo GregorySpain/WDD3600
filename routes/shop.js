5ab540ce51f8a70551d90430e55aa7594fb347e0
Gregory Spain <spaing3850@clarkstate.edu>
2022-02-04 20:10:58 -0500

    chapter 6

--- routes/shop.js
+++ routes/shop.js
@@ -5,9 +5,11 @@ const express = require('express');
 const rootDir = require('../util/path');
 
 const router = express.Router();
+const adminData = require('./admin')
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    const products = adminData.products;
+    res.render('shop', {pageTitle: 'My Shop', prods: products, path: '/a'})
 });
 
 module.exports = router;
