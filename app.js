361a62333734e7b10b638c31f109d1843d524df9
Gregory Spain <spaing3850@clarkstate.edu>
2022-02-05 19:51:38 -0500

    Chapter 7

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const get404 = require('./controllers/error')
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,18 +9,16 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: "Page Not Found"});
-});
+app.use(get404.get404);
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,14 +2,11 @@ const path = require('path')
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
-const adminData = require('./admin')
 
-router.get('/', (req, res, next) => {
-    const products = adminData.products;
-    res.render('shop', {pageTitle: 'My Shop', prods: products, path: '/a'})
-});
+
+router.get('/', productsController.getProducts);
 
 module.exports = router;
