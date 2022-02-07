af020bff11a5eaca5ed1d8bc2bad72f0a6f3d121
Gregory Spain <spaing3850@clarkstate.edu>
2022-02-06 20:11:11 -0500

    added comments to chapter 7

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,8 @@
+/**
+ * This code is a callback function that acts as the controller for a 404 error.
+ * It is called by the 404 router in the app.js file and renders the 404 page.
+ */
+
 exports.get404 = (req, res, next) => {
     res.status(404).render('404', {pageTitle: "Page Not Found", path: ''});
 }
--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,14 @@
+/**
+    This is the controller for returning and rendering product data as responses.
+    It gets data from the model, then passes it to the calling view. Each function
+    is a callback function executed by the routers.
+*/
+
+// import the model
 const Product = require('../models/product')
 
+// export the function that will render the add product view.
+// called by admin.js
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', {
         pageTitle: 'Add Product',
@@ -7,12 +16,22 @@ exports.getAddProduct = (req, res, next) => {
     });
 }
 
+// export the function that will execute on a post request.
+// this will get the product title and pass it as an parameter 
+// to create a Product object, then calls the save() method on that
+// object to store the title in the products.json
+// file, then redirect the user to the homepage.
+// called by admin.js
 exports.postAddProduct = (req, res, next) => {
     const product = new Product(req.body.title);
     product.save();
     res.redirect('/');
 }
 
+// export function to to display products.
+// It calls the static fetchAll() function of the Product class to get data
+// from the products.json file then render the view.
+// called by the shop router in shop.js.
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products) => {
         res.render('shop', { pageTitle: 'My Shop',
