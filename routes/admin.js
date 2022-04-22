ab4816158ca2193980bf433daf904b436a000b6e
Gregory Spain <spaing3850@clarkstate.edu>
2022-04-22 13:24:09 -0400

    chapter 18

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const express = require('express');
-
+const { check, body } = require('express-validator');
 const adminController = require('../controllers/admin');
 const isAuth = require('../middleware/is-auth');
 
@@ -14,11 +14,45 @@ router.get('/add-product', isAuth, adminController.getAddProduct);
 router.get('/products', isAuth, adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', isAuth, adminController.postAddProduct);
+router.post('/add-product',
+    isAuth,
+    [
+        body('title')
+            .trim()
+            .isLength({min: 3})
+            .isString(),
+        body('imageUrl')
+            .trim()
+            .isURL(),
+        body('price')
+            .trim()
+            .isFloat(),
+        body('description')
+            .trim()
+            .isLength({min: 5})
+    ],
+    adminController.postAddProduct);
 
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
-router.post('/edit-product', isAuth, adminController.postEditProduct);
+router.post('/edit-product',
+    isAuth,
+    [
+        body('title')
+            .trim()
+            .isLength({min: 3})
+            .isString(),
+        body('imageUrl')
+            .trim()
+            .isURL(),
+        body('price')
+            .trim()
+            .isFloat(),
+        body('description')
+            .trim()
+            .isLength({min: 5, max: 400})
+    ],
+    adminController.postEditProduct);
 
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
